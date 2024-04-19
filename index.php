4ee2d8651553f342298133979c2f7d37e92d4328
Nguyện <102305094+ndn2312@users.noreply.github.com>
2024-04-19 07:32:14 +0000

    bt

--- index.php
+++ index.php
@@ -1,32 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-///*----------------------------------------------------------------------------------------
-// * Copyright (c) Microsoft Corporation. All rights reserved.
-// * Licensed under the MIT License. See LICENSE in the project root for license information.
-// *---------------------------------------------------------------------------------------*/
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
+<body>
+    <?php
 
-?>
+    for ($i = 1; $i <= 200; $i++) {
+        if ($i % 2 == 0) {
+            echo "<span style='color: red; font-weight: bold; font-size:20px;'>{$i}</span>";
+        } else {
+            echo "<span style='color: blue; font-style: italic;font-size:20px'>{$i}</span>";
+        }
+        echo " ";
+    }
 
-<html>
-	<head>
-		<title>Advanced Web Development K15-N03</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('K15-N03 2024');
-			
-	
-			
-		?>
-		<form action="checkLogin.phd" method="POST">
-		<a>Username:</a><input type=text name="userName" size =16>
-		<a>Password:</a><input type=password name="passWord" size =16>
-		<input type=submit name=submit value="Login">
-		</form>
-	</body>
+    ?>
+
+</body>
+
 </html>
