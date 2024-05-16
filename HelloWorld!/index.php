661dd1c93e37c77906b8185171fbfdf44a6851a4
ndn2312 <kocoten001a@gmail.com>
2024-05-16 10:07:35 +0700

    CMS

---
+++ HelloWorld!/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World với Hình ảnh</title>
+    <style>
+    .container {
+        position: relative;
+        width: fit-content;
+        padding: 20px;
+    }
+
+    .text-over-image {
+        font-size: 50px;
+        padding-bottom: 30px;
+
+        color: red;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="text-over-image"><?php echo "Hello, World"; ?></div>
+        <img src="https://yt3.googleusercontent.com/Y9cP3_RcvcuAd8R8vH3M1xA2ERDRjqWV1vhsevgtMXTdp1UfCAcPKdKCpRObWR83nSgU8KOcPw=s900-c-k-c0x00ffffff-no-rj"
+            alt="Hình ảnh chào mừng" style="max-width: 300px;">
+
+    </div>
+</body>
+
+</html>
